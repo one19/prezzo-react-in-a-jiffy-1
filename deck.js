8b1a6a570e616742418067a4d8e58ec8f9d0f17b
Drew Showalter <maelstroswim@gmail.com>
2018-09-03 22:25:44 +1000

    :sparkles: split out and refine intro slides.

---
+++ deck.js
@@ -0,0 +1,9 @@
+export { future as theme } from 'mdx-deck/themes'
+
+import preamble from './slides/preamble.mdx';
+import intro from './slides/intro.mdx';
+
+export default [
+  ...preamble,
+  ...intro,
+];
